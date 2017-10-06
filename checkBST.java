ad123d9b99d2162cb885c289732f3ed55d8f3ff9
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2017-10-05 23:29:58 -0500

    bst

---
+++ checkBST.java
@@ -0,0 +1,20 @@
+boolean checkBST(Node root, int min, int max){
+
+        if(root == null){
+
+            return true;
+        }
+
+        if(root.data < min || root.data > max){
+
+            return false;
+
+        }
+
+        return checkBST(root.left, min, root.data-1) && checkBST(root.right, root.data+1, max);
+    }
+    boolean checkBST(Node root) {
+
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+    }
