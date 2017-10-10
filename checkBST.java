837f67005706f8f4baa5ee1cf6f3fbac0fa05a14
Ibrahim Ali <ibrahim.a.ali@vanderbilt.edu>
2017-10-10 12:25:21 -0500

    Update checkBST.java

--- checkBST.java
+++ checkBST.java
@@ -1,16 +1,19 @@
 boolean checkBST(Node root, int min, int max){
 
+        //base case 
         if(root == null){
 
             return true;
         }
 
+        //check exceptions to the rule because we don't want it to return if its true -- we need to check other cases 
         if(root.data < min || root.data > max){
 
             return false;
 
         }
 
+        // recursive step -- the node to the left cannot be greater than our current node and node to the right has to be greater than our current node
         return checkBST(root.left, min, root.data-1) && checkBST(root.right, root.data+1, max);
     }
 
