df549dfd1cf99a698d9b3fc1015ac10c3a56f11d
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2017-11-04 02:15:07 -0500

    fibonacci and merge linked lists

--- CTCI Hackerrank/fibonacci.java
+++ CTCI Hackerrank/fibonacci.java
@@ -3,15 +3,16 @@
 //2) base cases are 1 and 0. in those cases just return 1 or 0
 //3) recurse through with fibonacci(n-1) and fibonacci(n-2). this is because the next term is the sum of the two previous terms
 
-public static int fibonacci(int n) {
-
-        //establish base cases
-        if(n==1){
-            return 1;
-        } else if(n==0){
-            return 0;
-        }
-
-        //recursive line
-        return fibonacci(n-1) + fibonacci(n-2);
+public static int fib(int n)
+{
+    int a = 0, b = 1, c;
+    if (n == 0)
+        return a;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
     }
+    return b;
+}
--- Microsoft Prep/mergeTwoLinkedList.java
+++ Microsoft Prep/mergeTwoLinkedList.java
@@ -6,6 +6,39 @@
 //4) now we need to get the next node, so result.next = mergeTwoLists. here we only need to increment the node we set our result to
 //5) finally, return the result
 
+//iterative solution -- honestly much easier to understand
+
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0);
+        ListNode copy = head;
+
+        while(l1 != null || l2 != null){
+
+            if(l1 != null && l2 != null){
+                if(l1.val < l2.val){
+                    head.next = l1;
+                    l1 = l1.next;
+                }else{
+                    head.next = l2;
+                    l2 = l2.next;
+                }
+                head = head.next;
+            }else if(l1 != null && l2 == null){
+                head.next = l1;
+                l1 = l1.next;
+                head = head.next;
+            } else{
+                head.next = l2;
+                l2 = l2.next;
+                head = head.next;
+            }
+        }
+
+        return copy.next;
+
+}
+//recursive solution
 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null){
             return l2;
